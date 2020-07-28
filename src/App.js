4d6f41cbb194bc7af165b2f8e0d0fa02c57d0c1e
Andrew Radulescu <radulescuandrew@gmail.com>
2020-07-29 00:44:01 +0300

    test v2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Salut <code>Radu</code>, this is CI/CD in AWS Amplify!
         </p>
         <a
           className="App-link"
